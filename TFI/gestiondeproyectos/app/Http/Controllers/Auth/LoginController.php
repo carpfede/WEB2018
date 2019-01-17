8ce1f6b546ecbd10ae11f9e0e515ddf470ecacce
carpfede <fedefiasare@gmail.com>
2019-01-17 13:29:23 -0300

    adapt to user service

--- TFI/gestiondeproyectos/app/Http/Controllers/Auth/LoginController.php
+++ TFI/gestiondeproyectos/app/Http/Controllers/Auth/LoginController.php
@@ -7,62 +7,31 @@ use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
 use Brian2694\Toastr\Facades\Toastr;
 use Illuminate\Support\Facades\Hash;
+use App\Application\Services\UserService;
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
-
     use AuthenticatesUsers;
 
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/home';
+    private $service;
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    public function __construct(UserService $service)
     {
+        $this->service = $service;
         $this->middleware('guest')->except('logout');
-    }
-
-    public function username()
-    {
-        return 'username';
     }
 
     public function login(Request $request)
     {
         $username = $request->username;
         $password = $request->password;
-        // Get user data
     
-        $user = \App\Domain\User::where('username', $username)
-                ->where('disabled', false)
-                ->first();
-
+        $user = $this->service->findOne($username);
         
-        // Check
         if (!empty($user) && Hash::check($password, $user->password)) {
-            // logged in
             Toastr::success('', 'Logeo exitoso', ["positionClass" => "toast-bottom-right"]);
             return view('home');
         } else {
-            // Eorror
             Toastr::error('Credenciales invalidas', 'Error de autenticación', ["positionClass" => "toast-bottom-right"]);
             return view('auth.login');
         }
