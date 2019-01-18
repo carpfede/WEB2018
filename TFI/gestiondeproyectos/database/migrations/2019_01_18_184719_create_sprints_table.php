f09129c6a31f71fc57b8d43753e70032b024b6e9
carpfede <fedefiasare@gmail.com>
2019-01-18 15:51:54 -0300

    migration for sprints

---
+++ TFI/gestiondeproyectos/database/migrations/2019_01_18_184719_create_sprints_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSprintsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sprints', function (Blueprint $table) {
+            $table->increments('id');
+            $table->intger('number');
+            $table->string('version');
+            $table->date('from');
+            $table->date('toEstimated');
+            $table->date('to')->nullable();
+            $table->unsignedInteger('project_id');
+            $table->foreign('project_id')->references('id')->on('projects');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sprints');
+    }
+}
