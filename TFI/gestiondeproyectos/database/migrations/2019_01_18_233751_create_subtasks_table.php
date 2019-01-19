33e272225567689c7e42a366887061011681316b
Federico Gabriel Fia Sare <carpfede>
2019-01-18 22:56:47 -0300

    migration for subtasks

---
+++ TFI/gestiondeproyectos/database/migrations/2019_01_18_233751_create_subtasks_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSubtasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subtasks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->time('estimated');
+            $table->time('remaining');
+            $table->enum('status', ['Hacer', 'EnProgreso', 'Resuelta', 'Testing', 'Cancelada']);
+            $table->unsignedInteger('member_id');
+            $table->foreign('member_id')->references('id')->on('members');
+            $table->unsignedInteger('task_id');
+            $table->foreign('task_id')->references('id')->on('tasks');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subtasks');
+    }
+}
